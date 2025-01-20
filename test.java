165665521d7c2314ded0d5aaf716f10a43fde1a3
notYouralex <alexanderlarche02@gmail.com>
2025-01-20 13:57:07 +0800

    commit3

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("monster jerbin");
+        System.out.println("monster doydoy");
         int x = 10;
     }
 }
